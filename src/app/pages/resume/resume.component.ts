7486432744d2eb4c9e2ccaeebc0d901ec77edda1
Marcos Chanca <daxter2424@hotmail.com>
2022-06-26 14:52:03 +0200

    bio & resume page

--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
-  styles: [
-  ]
+  styleUrls: [`./resume.component.css`]
 })
 export class ResumeComponent implements OnInit {
 
