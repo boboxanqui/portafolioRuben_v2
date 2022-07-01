ffa31ab51da839562fead043c918630bd740f242
Marcos Chanca <daxter2424@hotmail.com>
2022-07-01 21:34:03 +0200

    setting swiper

--- src/app/pages/book/book.component.ts
+++ src/app/pages/book/book.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import SwiperCore, { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  styles: [
-  ]
+  styleUrls: [ './book.component.css' ]
 })
-export class BookComponent implements OnInit {
 
-  constructor() { }
+export class BookComponent  {
 
-  ngOnInit(): void {
+  constructor() {
+    for( let i = 1; i <= this.numPics; i++ ){
+      this.pics.push(`../../../assets/img/galeria/galeria-${i}.webp`)
+    }
+   }
+
+  numPics: number = 18;
+  pics: string[] = []
+
+  config: SwiperOptions = {
+    slidesPerView: 3,
+    spaceBetween: 50,
+    navigation: true,
+    pagination: { clickable: true },
+    scrollbar: { draggable: true },
+  }
+
+  onSwiper( [swiper]: any ){
+    console.log(swiper);
+  }
+
+  onSlideChange() {
+    console.log('slide change');
   }
 
 }
