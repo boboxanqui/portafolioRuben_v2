4a5fb3be261b7653c3cf6e18e745338b78baf518
Marcos Chanca <daxter2424@hotmail.com>
2022-07-16 12:37:06 +0200

    zoom in & zoom out buttons enabled

--- src/app/pages/swiper/pictureViewer.component.ts
+++ src/app/pages/swiper/pictureViewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, NgZone, OnInit, ViewChild } from '@angular/core';
 import SwiperCore, { SwiperOptions, Keyboard, Navigation, Pagination, Zoom, Swiper } from 'swiper';
 import { SwiperComponent } from "swiper/angular";
 import { NgbActiveModal, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
@@ -12,23 +12,24 @@ SwiperCore.use([Keyboard, Navigation, Pagination, Zoom]);
   templateUrl: './pictureViewer.component.html',
   styleUrls: ['./pictureViewer.component.scss']
 })
+
 export class pictureViewerComponent implements OnInit {
 
   @Input() slide: number = 0;
   @ViewChild('swiper', {static: false}) swiper?: SwiperComponent;
 
-  constructor( private modalService: NgbModal ) { }
-
+  constructor( 
+    private modalService: NgbModal,
+    private ngZone: NgZone
+    ) { }
+  
   ngOnInit(): void {
-    console.log('Swiper component creation');
     this.config.initialSlide = this.slide
   }
 
-  // private swiper = new Swiper('.swiper');
-
   config: SwiperOptions = {
     centeredSlides: true,
-    
+    centeredSlidesBounds: true,
     init: false,
     initialSlide: 0,
     keyboard: true,
@@ -37,36 +38,33 @@ export class pictureViewerComponent implements OnInit {
     pagination: { dynamicBullets: true },
     slidesPerView: 1,
     spaceBetween: 50,
-    zoom: {maxRatio: 2},
+    touchEventsTarget: 'container',
+    zoom: { maxRatio: 2 },
   }
 
+  private _zoomed!: boolean;
+
+  get zoomed():boolean{
+    return this._zoomed
+  }
+  
   closeSwiper(){
     this.modalService.dismissAll()
   }
 
   zoomIn(){
-    console.log('zoooOOOOO00000OOOOOOooooo.....m');
-
     this.swiper?.swiperRef.zoom.in()
-
-    console.log(    this.swiper?.swiperRef.zoom.scale
-      );
-    
-
-
-    // this.swiper.config.zoom = {minRatio: 3}
-
-    // console.log(this.swiper.zoom.scale);
-
   }
 
   zoomOut(){
     this.swiper?.swiperRef.zoom.out()
   }
 
-  zoomed():boolean{
-    return this.swiper?.swiperRef.zoom.scale! >= 2
+  zoomChange( event: any ){
+    this.ngZone.run( (..._) =>{
+      event[1] === 1 ? this._zoomed = false : this._zoomed = true;
+      // console.log( 'IS IN ANGULAR ZONE -> ', NgZone.isInAngularZone() );
+    } )
   }
-
 
 }
