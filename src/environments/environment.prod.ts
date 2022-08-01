eaa070a3178b6e5b96a88e566b6eaf0a18976c81
Marcos Chanca <daxter2424@hotmail.com>
2022-08-01 09:37:02 +0200

    After v1.1.0 update & Budget at 2mb

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  reCaptcha: {
+    webKey: '6Le1KSkhAAAAAKnIxCBl3P-nQuUnAW39cO4OCYkT',
+  },
+  smtp: {
+    secureToken: 'e3f76226-a3ef-4a8d-80c8-8ce9d12ae767'
+  }
 };
